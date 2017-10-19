86b32fda9a3f3f3c9a5f9e65dca7c44551310b94
LAPTOP-UBLE746G\Alejandra Santos <alejandra.santos.97@hotmail.com>
2017-10-18 22:22:40 -0500

    Practica2

---
+++ src/txtapdf/TxtAPdf.java
@@ -0,0 +1,18 @@
+
+package txtapdf;
+
+/**
+ *
+ * @author Jaime
+ */
+public class TxtAPdf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame g = new JFrame(); 
+        g.setVisible(true);
+    }
+    
+}
